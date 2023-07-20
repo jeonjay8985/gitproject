6e64d656d4fd48cb1d9856153baad4e41704dc39
yj-603-19 <yj-603-19@DESKTOP-4I99M6O>
2023-07-20 09:27:46 +0900

    깃 프로젝트

--- src/gitproject/hello.java
+++ src/gitproject/hello.java
@@ -3,7 +3,8 @@ package gitproject;
 public class hello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Helloworld!!");
+		System.out.println("헬로월드!!");
 
 	}
 
